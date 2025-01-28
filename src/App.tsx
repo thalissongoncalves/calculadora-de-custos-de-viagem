3ba8d2104c519c66727537f022a7890baf67283f
thalissongoncalves <thalissongsilva70@gmail.com>
2025-01-28 12:42:02 -0300

    Adicionado a logotipo no cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import logoImg from './assets/logo.jpg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <header>
+        <img 
+          className='logoImg'
+          src={logoImg} 
+          alt="Logotipo de um carro no posto de combustível com um relógio em cima escrito TRAVEL CALCULATOR" 
+        />
+      </header>
     </>
   )
 }
